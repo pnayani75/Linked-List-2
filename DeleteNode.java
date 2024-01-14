73dd36a617dc9dc68d33ce20d4ddcaac384053eb
pnayani75 <35510785+pnayani75@users.noreply.github.com>
2024-01-14 03:36:36 -0800

    Create DeleteNode.java

---
+++ DeleteNode.java
@@ -0,0 +1,9 @@
+class DeleteNode {
+    public void deleteNode(ListNode node) {
+            node.val=node.next.val;
+            node.next=node.next.next;
+            
+        
+        
+    }
+}
